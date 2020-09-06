34ad03e31cfe532349dd25e9ac753c722742ddba
Yinan Liang <yinan.liang@coles.com.au>
2020-09-06 20:31:19 +1000

    use template for frontend

---
+++ client/src/routes.js
@@ -0,0 +1,40 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.9.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import DashboardPage from "views/Dashboard/Dashboard.js";
+import TableList from "views/TableList/TableList.js";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin",
+  },
+  {
+    path: "/table",
+    name: "Table List",
+    icon: "content_paste",
+    component: TableList,
+    layout: "/admin",
+  },
+];
+
+export default dashboardRoutes;
